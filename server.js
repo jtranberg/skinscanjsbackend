55aea3337a6b9377c0aad3402f1312cceb1029b2
jtranberg <jtranberg@hotmail.ca>
2025-04-26 11:38:52 -0700

    commit

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.post('/login', async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
-    if (!user || !(await bcrypt.compare(password, user.password))) {
+    if (!user || !bcrypt.compare(password, user.password)) {
       return res.status(401).json({ message: 'Invalid credentials' });
     }
     res.status(200).json({ message: 'Login successful', email });
